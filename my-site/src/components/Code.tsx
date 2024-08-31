6993f03cd088f9d18caa7a3d1fef5cf1cdaa590e
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-31 18:00:19 +0300

    made a blinking cursor

--- my-site/src/components/Code.tsx
+++ my-site/src/components/Code.tsx
@@ -1,18 +1,19 @@
 export default function Code() {
   return (
-    <div className="codeContainer bg-gray-900 rounded-lg overflow-hidden shadow-lg max-w-md">
+    <div className="codeContainer bg-gray-900 rounded-lg overflow-hidden shadow-lg w-10/12">
       <div className="bg-gray-800 px-4 py-2 flex items-center">
         <div className="h-3 w-3 bg-red-500 rounded-full mr-2"></div>
         <div className="h-3 w-3 bg-yellow-500 rounded-full mr-2"></div>
         <div className="h-3 w-3 bg-green-500 rounded-full"></div>
       </div>
-      <div className="px-8 py-8 border-t-[2px] border-indigo-900 overflow-hidden">
+      <div className="px-8 py-8 border-t-[2px] border-indigo-900 overflow-hidden flex items-center">
         <code className="font-mono text-sm leading-loose">
           <div className="blink">
             <span className="text-pink-500 mr-2">const</span>
             <span className="text-white mr-2">coder</span>
             <span className="text-pink-500 mr-2">=</span>
-            <span className="text-gray-400"></span>
+            <span className="text-gray-400">{`{ `}</span>
+            <span className="cursor"></span>
           </div>
           <div>
             <span className="text-white ml-8 mr-2">{`{`}name:</span>
@@ -42,6 +43,7 @@ export default function Code() {
             <span className="text-green-400 ml-8 mr-2">hireable:</span>
             <span className="text-orange-400">function</span>
             <span className="text-gray-400">()</span>
+            <span className="text-gray-400">{` {`}</span>
           </div>
           <div>
             <span className="text-orange-400 ml-16 mr-2">return</span>
@@ -64,13 +66,13 @@ export default function Code() {
             <span className="text-orange-400">5</span>
           </div>
           <div>
-            <span className="text-gray-400 ml-16 mr-2">){`}`};</span>
+            <span className="text-gray-400 ml-16 mr-2">);</span>
           </div>
           <div>
-            <span className="text-gray-400 ml-8"></span>
+            <span className="text-gray-400 ml-8">{`}`}</span>
           </div>
           <div>
-            <span className="text-gray-400"></span>
+            <span className="text-gray-400">{`}`}</span>
           </div>
         </code>
       </div>
--- my-site/src/components/Home.tsx
+++ my-site/src/components/Home.tsx
@@ -3,8 +3,10 @@ import Code from "./Code";
 export default function Home() {
   return (
     <div className="homeContainer">
-      Hello
-      <Code></Code>
+      <span className="homeText">Hello</span>
+      <div className="homeCode">
+        <Code></Code>
+      </div>
     </div>
   );
 }
