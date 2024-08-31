1151670f02d02209d448ea432f02ff66b99fed53
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-31 17:08:28 +0300

    navbar fixed

--- my-site/src/App.tsx
+++ my-site/src/App.tsx
@@ -1,10 +1,11 @@
 import Home from "./components/Home"
+import Navigation from "./components/Navigation"
+import './styles/base.css'
 
 function App() {
-
-
   return (
     <>
+      <Navigation/>
       <Home></Home>
     </>
   )
--- my-site/src/components/Home.tsx
+++ my-site/src/components/Home.tsx
@@ -1,7 +1,10 @@
+import Code from "./Code";
+
 export default function Home(){
     return (
         <div className="homeContainer">
             Hello
+            <Code></Code>
         </div>
     )
 }
