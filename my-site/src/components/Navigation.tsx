a1f2354b1748fcf3588adf5d69b3fc7100e83765
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-01 14:31:37 +0300

    nav fix to open nav on phone

--- my-site/src/components/Navigation.tsx
+++ my-site/src/components/Navigation.tsx
@@ -1,4 +1,8 @@
-import { Disclosure, DisclosureButton } from "@headlessui/react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import "../styles/navigation.css";
 import logo from "../assets/logo/S1_transparent-.png";
@@ -66,6 +70,26 @@ export default function Navigation() {
           </div>
         </div>
       </div>
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
     </Disclosure>
   );
 }
