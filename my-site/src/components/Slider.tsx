41259db14556e6e69507510ea7ba975b036d5007
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-02 15:31:06 +0300

    slider functions added

--- my-site/src/components/Slider.tsx
+++ my-site/src/components/Slider.tsx
@@ -7,8 +7,22 @@ type ImageSliderProps = {
 export default function Slider({ imgUrls }: ImageSliderProps) {
   const [imageIndex, setimageIndex] = useState(0);
 
-  function previousImage() {}
-  function nextImage() {}
+  function previousImage() {
+    setimageIndex((index) => {
+      if (index === 0) {
+        return imgUrls.length - 1;
+      }
+      return index - 1;
+    });
+  }
+  function nextImage() {
+    setimageIndex((index) => {
+      if (index === imgUrls.length - 1) {
+        return 0;
+      }
+      return index + 1;
+    });
+  }
   return (
     <div className="sliderContainer">
       <img src={imgUrls[imageIndex]} alt={`Slide ${imageIndex}`} />
