779ddfbc98147d9ed60d064bcfe5f9002ef70927
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-03 18:23:09 +0300

    change

--- my-site/src/components/Projects.tsx
+++ my-site/src/components/Projects.tsx
@@ -1,7 +1,9 @@
 export default function Projects() {
   return (
     <div className="projectsContainer">
-      <div className="projectsContainer2">Hello</div>
+      <div className="projectsContainer2">
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 }
