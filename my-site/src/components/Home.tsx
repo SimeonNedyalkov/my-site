cd17c721a650398d0c008abb48ebe807ba746320
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-05 21:51:56 +0300

    added 2 new projects

--- my-site/src/components/Home.tsx
+++ my-site/src/components/Home.tsx
@@ -1,6 +1,8 @@
 import img1 from "../assets/projects/Screenshot 2024-09-01 155000.png";
 import img2 from "../assets/projects/Screenshot 2024-09-01 155050.png";
 import img3 from "../assets/projects/Screenshot 2024-09-01 161416.png";
+import img4 from "../assets/projects/Screenshot 2024-09-05 212729.png";
+import img5 from "../assets/projects/Screenshot 2024-09-05 212941.png";
 import Code from "./Code";
 import Slider from "./Slider";
 
@@ -9,6 +11,8 @@ export default function Home() {
     { url: img1, alt: "Image1" },
     { url: img2, alt: "Image2" },
     { url: img3, alt: "Image3" },
+    { url: img4, alt: "Image4" },
+    { url: img5, alt: "Image5" },
   ];
 
   return (
