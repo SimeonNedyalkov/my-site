08a6156b8ed0549471aaf11f4db943a12574a8c7
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-06 08:22:02 +0300

    Update Projects.tsx
    Change

--- my-site/src/components/Projects.tsx
+++ my-site/src/components/Projects.tsx
@@ -2,7 +2,8 @@ export default function Projects() {
   return (
     <div className="projectsContainer">
       <div className="projectsContainer2">
-        <h1 className="projectsHello">Hello</h1>
+        <h1 className="projectsHello">Hello, those are my projects: </h1>
+        <div>Projects</div>
       </div>
     </div>
   );
