87f2af5ca91be74f9ab211f535eb69926ea4e740
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-06 08:18:34 +0300

    Update Projects.tsx
    Classname added

--- my-site/src/components/Projects.tsx
+++ my-site/src/components/Projects.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <div className="projectsContainer">
       <div className="projectsContainer2">
-        <h1>Hello</h1>
+        <h1 className="projectsHello">Hello</h1>
       </div>
     </div>
   );
