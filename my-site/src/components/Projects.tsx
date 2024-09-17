333ba1ed83153a5faa23ce376b7bd2b4865da49d
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-17 19:21:15 +0300

    projects modified

--- my-site/src/components/Projects.tsx
+++ my-site/src/components/Projects.tsx
@@ -3,6 +3,7 @@ export default function Projects() {
     <div className="projectsContainer">
       <div className="projectsContainer2">
         <h1>Hello</h1>
+        <h1>Check projects here</h1>
       </div>
     </div>
   );
