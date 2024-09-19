ef58f5b79785d38ad9688830c878e7b78e8496a8
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-09-19 18:13:32 +0300

    Bio component created

---
+++ my-site/src/components/Bio.tsx
@@ -0,0 +1,11 @@
+export default function Bio() {
+  return (
+    <div>
+      <h1>Bio</h1>
+      <span>
+        Simeon Nedyalkov studied Software Engineering at Software University,
+        from which he gratuated in 2024.{" "}
+      </span>
+    </div>
+  );
+}
