930c8c334cc0ce3388d6bf0bef55eb208ccf7641
SimeonNedyalkov <simeon_sg@abv.bg>
2024-11-27 00:35:12 +0200

    button added for a cv

--- my-site/src/components/Bio.tsx
+++ my-site/src/components/Bio.tsx
@@ -10,12 +10,20 @@ export default function Bio() {
       className={isVisible ? "dynamicBackground text-7xl" : "text-7xl"}
     >
       <h1>Bio</h1>
-      <p className="mt-6">
+      <p className="mt-6 text-lg">
         I'm Simeon Nedyalkov, a JavaScript Web Development graduate from
-        Software University, class of 2024. Passionate about creating modern,
-        responsive web applications, I focus on building clean and efficient
-        code.
+        Software University, class of 2024. Currently studying Software
+        Engineering and Management in Varna Free University.<br></br> I am
+        passionate about creating modern, responsive web applications. <br></br>{" "}
+        I focus on building clean and efficient code.
       </p>
+      <a
+        href="https://drive.google.com/file/d/1FR4rM8yiobFs32aBGIov4hcI2gJkd0sK/view?usp=drive_link"
+        target="_blank"
+        className="button"
+      >
+        Check CV
+      </a>
     </div>
   );
 }
