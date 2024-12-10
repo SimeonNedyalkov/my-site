90f498c733b93abce8539def9e2be9c85c9e9043
SimeonNedyalkov <simeon_sg@abv.bg>
2024-12-10 14:11:34 +0200

    lenion screenshot and data added

--- my-site/src/components/Home.tsx
+++ my-site/src/components/Home.tsx
@@ -1,9 +1,10 @@
-import img1 from "../assets/projects/Screenshot 2024-09-01 155000.png";
-import img2 from "../assets/projects/Screenshot 2024-09-01 155050.png";
-import img3 from "../assets/projects/Screenshot 2024-09-01 161416.png";
-import img4 from "../assets/projects/Screenshot 2024-09-05 212729.png";
-import img5 from "../assets/projects/Screenshot 2024-09-05 212941.png";
-import img6 from "../assets/projects/Screenshot 2024-10-22 101706.png";
+import img1 from "../assets/projects/Screenshot 2024-12-09 133926.png";
+import img2 from "../assets/projects/Screenshot 2024-09-01 155000.png";
+import img3 from "../assets/projects/Screenshot 2024-09-01 155050.png";
+import img4 from "../assets/projects/Screenshot 2024-09-01 161416.png";
+import img5 from "../assets/projects/Screenshot 2024-09-05 212729.png";
+import img6 from "../assets/projects/Screenshot 2024-09-05 212941.png";
+import img7 from "../assets/projects/Screenshot 2024-10-22 101706.png";
 import githubIco from "../assets/contact-icons/github-svgrepo-com.svg";
 import linedInIco from "../assets/contact-icons/linkedin-svgrepo-com.svg";
 import outlook from "../assets/contact-icons/mail-svgrepo-com.svg";
@@ -43,9 +44,17 @@ export default function Home() {
 
   const arrayOfProjects = [
     {
+      name: "Lenion",
+      desc: '" Lenion " is React-based application, written in Typescript and SCSS with a futuristic design, that utilising three.js / react/three-fiber to build a futuristic 3d clothes shop. With the help of redux to manage the store and state.',
+      url: img1,
+      alt: "Image1",
+      demoUrl: "no",
+      projectCode: "https://github.com/SimeonNedyalkov/clothing-store",
+    },
+    {
       name: "Cat Date",
       desc: '" Cat-Date " is a fun and interactive Angular-based application, inspired by Tinder, designed exclusively for connecting cats and their owners. Swipe right to find the perfect playmate for your feline friend, schedule playdates, and join a community of cat enthusiasts!',
-      url: img1,
+      url: img2,
       alt: "Image1",
       demoUrl: "https://cat-date-client.vercel.app/",
       projectCode: "https://github.com/SimeonNedyalkov/Cat-Date-Angular",
@@ -53,7 +62,7 @@ export default function Home() {
     {
       name: "Choose Your Figher",
       desc: '" Choose Your Fighter " is a React project where users pick characters like bears, sharks, octopuses, and eagles to battle against the app itself. It demonstrates React skills, interactive UI design, state management, and game logic implementation for an engaging gaming experience.',
-      url: img2,
+      url: img3,
       alt: "Image2",
       demoUrl: "https://choose-your-fighter-client.vercel.app/",
       projectCode:
@@ -62,7 +71,7 @@ export default function Home() {
     {
       name: "Magma Heaven",
       desc: '" Magma Heaven " is an app made with Node.js, Express.js, mongoose, cookie-parser, jsonwebtoken, handlebars, bcypt, express-validators.',
-      url: img3,
+      url: img4,
       alt: "Image3",
       demoUrl: "no",
       projectCode: "https://github.com/SimeonNedyalkov/02.MagmaHeaven",
@@ -70,7 +79,7 @@ export default function Home() {
     {
       name: "Earth Treasure Vault",
       desc: '" Earth Treasure Vault " is an app made on NodeJs with express, mongoose, cookie-parser, jsonwebtoken, handlebars, bcypt and express-validators',
-      url: img4,
+      url: img5,
       alt: "Image4",
       demoUrl: "no",
       projectCode: "https://github.com/SimeonNedyalkov/01.EarthTreasureVault",
@@ -78,7 +87,7 @@ export default function Home() {
     {
       name: "Movie Magic",
       desc: '" Movie Magic " is an app made with Node.js, Express.js, mongoose, cookie-parser, jsonwebtoken, handlebars, bcypt, express-validators.',
-      url: img5,
+      url: img6,
       alt: "Image5",
       demoUrl: "no",
       projectCode: "https://github.com/SimeonNedyalkov/movie-magic",
@@ -86,7 +95,7 @@ export default function Home() {
     {
       name: "Restaurant Finder",
       desc: '" Restaurant Finder " is a React-based app that helps you discover the top-rated restaurants in Varna with just a few simple clicks. Powered by the Google API, it delivers fast and accurate results, making it easy to find the best dining spots in the city.',
-      url: img6,
+      url: img7,
       alt: "Image6",
       demoUrl: "no",
       projectCode: "https://github.com/SimeonNedyalkov/restaurant-finder-react",
